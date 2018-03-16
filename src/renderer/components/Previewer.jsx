321c90a6eb87b0fbb76b5387cc419c570f110c7c
reign <reignofmiracle@gmail.com>
2018-03-16 20:01:35 +0900

    add react-contenteditable

--- src/renderer/components/Previewer.jsx
+++ src/renderer/components/Previewer.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import marked from "marked"
+import ContentEditable from 'react-contenteditable'
 import style from "./Previewer.css"
 
 marked.setOptions({ sanitize: true})
@@ -7,10 +8,7 @@ marked.setOptions({ sanitize: true})
 export default function Previewer(props) {
     return (
         <div id="previewer" className={`${props.className} ${style.previewer}`} >
-            <div 
-                dangerouslySetInnerHTML={{ __html: marked(props.value) }} 
-                contenteditable="true"                
-                />
+            <ContentEditable html={marked(props.value)} disabled={true} />
         </div>
     )
 }
